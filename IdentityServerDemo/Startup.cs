236a1b894ea51d2507410a938fddc30aac3d26df
Matheus Rodrigues <email.matheusgr@gmail.com>
2024-01-05 16:48:48 -0300

    Add project files.

---
+++ IdentityServerDemo/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace IdentityServerDemo;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddIdentityServer()
+            .AddInMemoryClients(IdentityConfiguration.Clients)
+            .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+            .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+            .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+            .AddTestUsers(IdentityConfiguration.TestUsers)
+            .AddDeveloperSigningCredential();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseIdentityServer();
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddAuthentication("Bearer")
+            .AddIdentityServerAuthentication("Bearer", options =>
+            {
+                options.ApiName = "myApi";
+                options.Authority = "https://localhost:7039";
+            });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
